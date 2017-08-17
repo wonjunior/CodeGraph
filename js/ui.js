b155b8b0ebc85445b328d8f0872a82d9affdff42
Ivan <ivan.lopes@live.fr>
2017-08-17 20:53:04 +0200

    implentation of the 'exe' docks as well as some refactoring.

--- js/ui.js
+++ js/ui.js
@@ -19,55 +19,46 @@ class UserInterface {
 
     }
 
+    // NEED A CLASS FOR FORMATTING RELATED STUFF!
     formatPos(pos) {
-
         return 'style="left:' + pos[0] + 'px;top:' + pos[1] + 'px"';
-
     }
 
-    /** !!!!!!! this needs to go somewhere else !!!!!!!!!!! **/
+    formatDock(side, i, label) {
+        let prefix = (side == 'right') ? 'O' : 'I';
+        return '<div class="data '+prefix+'dock_'+i+ ' ' +side+'"><div class="snapDock" state=""></div><div class="dock"></div><div class="paramName">'+label+'</div></div>';
+    }
+
+    formatExeDock() {
+        return '<div class="execute"><div class="exe Edock_0 left"><div class="pin"></div><div class="snapDock" state=""></div></div><div class="exe Edock_1 right"><div class="pin"></div><div class="snapDock" state=""></div></div></div>';
+    }
+
+    formatHeader(pos, id, name) {
+        return '<div '+pos+'class="container '+id+'"><div class="header"><div class="headerTitle">'+name+'</div></div><div class="body">'
+    }
+
     getNodeHTML(node) {
 
-        var html;
+        var html = this.formatHeader(this.formatPos(node.position), node.id, node.name);
 
-        node.nbEntries = 0;
+        if (node.execution) {
 
-        var pos = this.formatPos(node.position);
+            html += this.formatExeDock();
 
-        var html = '<div '+pos+'class="container '+node.id+'"><div class="header"><div class="headerTitle">'+node.label+'</div></div><div class="body"><div class="leftblock">';
-
-        var dock = 0;
-
-        // add node's parameters
-        for(var i = 0; i < node.input.length; i++) {
+        } html += '<div class="leftblock">'
 
 
-            html += '<div class="parameter dock_'+String(++dock)+' left"><div class="snapDock" state=""></div><div class="dock"></div><div class="paramName">'+node.input[i]+'</div></div>';
+        for (let i in node.label.input) {
 
+            html += this.formatDock('left', String(i), node.label.input[i]);
 
-        }
-        node.totalNbEntries = dock;
+        } html += '</div><div class="rightblock">';
 
-        // input node
-        if(node.input.length == 0) {
-            node.nbEntries = node.argument.length;
-            node.totalNbEntries = node.argument.length;
-        }
+        for (let i in node.label.output) {
 
-        html += '</div><div class="rightblock">';
+            html += this.formatDock('right', String(i), node.label.input[i]);
 
-        // add node's output
-        for(var i = 0; i < node.output.length; i++) {
-
-            var dockId = 'dock_'+String(++dock);
-
-            node.return.push(node.id+'-'+dockId);
-
-            html += '<div class="parameter '+dockId+' right"><div class="snapDock" state=""></div><div class="dock"></div><div class="paramName">'+node.output[i]+'</div></div>';
-
-        }
-
-        html += '</div></div></div>';
+        } html += '</div></div></div>';
 
         return html;
 
