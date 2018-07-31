3595d0dabaad31519234cfdc7cce3a11e3ed6571
Ivan <ivan.lopes@live.fr>
2018-07-31 11:56:31 +0200

    Events are now linked to .snapDock, .dock are for visuals only.

--- refactoredJS/draggable.js
+++ refactoredJS/draggable.js
@@ -52,12 +52,12 @@ document.addEventListener('mousedown', event => {
 
     if (event.target.classList.contains('header') && event.button == 0) {
 
-        const nodeObject = node[event.target.ref];
+        const nodeObject = node[ event.target.ref ];
         new Draggable(event, nodeObject.nodeElement, nodeObject);
 
-    } else if (event.target.classList.contains('dock')) {
+    } else if (event.target.classList.contains('snapDock')) {
 
-        const dockObject = dock[event.target.ref];
+        const dockObject = dock[ event.target.ref ];
         new Linkable(event, dockObject);
 
     } else if (event.target.classList.contains('objects') && event.button == 2) {
