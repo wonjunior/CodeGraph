a1e7f897c53bd1e92fcdd56e2b4795d0ac5be450
Ivan <ivan.lopes@live.fr>
2019-01-29 08:28:22 +0100

    wait... `wait` is even shorter !

--- editor/functions.js
+++ editor/functions.js
@@ -2,11 +2,11 @@
   * Miscellaneous functions used throughout the code
   */
 
-// console.log() handy shortcut
+// console.log handy shortcut
 const _ = (...a) => console.log(...a);
 
 // determine if a string contains either no characters or only spaces
 const isEmpty = str => !str.replace(/\s/g, '').length;
 
 // adds the given function to the JS queue of execution
-const wait = fn => setTimeout(() => fn(), 0);
+const wait = fn => setTimeout(fn, 0);
