e8c4cfd85d653097f57c2bfa5fbc86cf4aab2b4a
Ivan <ivan.lopes@live.fr>
2019-02-02 22:28:02 +0100

    moved class Stream in seperate file

--- compiler/ControlFlow.js
+++ compiler/ControlFlow.js
@@ -4,6 +4,11 @@ class ControlFlow {
 
     static update(node) {
 
+        // <? check for cycles
+        // const cycleDetected = ~data.propagationTree.indexOf(node);
+
+        _(`[ControlFlow on ${node.id}]`);
+
         node instanceof Executable ? node.execute() : node.calculate();
 
     };
@@ -24,45 +29,6 @@ class ControlFlow {
             stream.calculate();
 
         } while (stream.next());
-
-    };
-
-}
-
-class Stream {
-
-    constructor(root) {
-
-        this.nodes = [ root ];
-
-    }
-
-    get current() {
-
-        return this.nodes[ this.nodes.length - 1 ];
-
-    }
-
-    set current(nextNode) {
-
-        this.nodes.push(nextNode);
-
-    };
-
-    calculate() {
-
-        this.current.calculate();
-
-    };
-
-    next() {
-
-        const exeDocks = this.current.exeDocks.out;
-        if (exeDocks.length && exeDocks[0].links.length) {
-
-            return this.current = exeDocks[0].links[0].endDock.node;
-
-        }
 
     };
 
