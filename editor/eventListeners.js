09e1394b1642ba965525c7e3734f6ca4ec33d09e
Ivan <ivan.lopes@live.fr>
2019-07-02 00:17:43 +0200

    event listeners in seperate file

---
+++ editor/eventListeners.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// captures all keyboard input on the document
+document.addEventListener('keyup', event => {
+
+    new KeyEventHandler(event, State.current);
+
+});
+
+// captures all mouse events (on any mouse button) on the document
+document.addEventListener('mousedown', event => {
+
+    new MouseEventHandler(event, State.current);
+
+});
+
+/*document.addEventListener('keydown', event => {
+
+    if (event.keyCode == 9) {
+
+        event.preventDefault();
+
+    }
+
+});*/
