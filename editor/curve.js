2683778a7956087f465082fc31f4bc13ded52a1f
Ivan <ivan.lopes@live.fr>
2019-07-06 20:02:48 +0200

    Documentation and some small changes to `Curve`

--- editor/curve.js
+++ editor/curve.js
@@ -1,62 +1,78 @@
 'use strict'
 
+/**
+ * Utility class to generate SVG paths
+ */
 class Curve {
 
-    static get([ startX, startY ], [ endX, endY ]) {
+	/**
+	 * Default properties for the path.
+	 */
+	static parameters = {
+		factor: 0.4,
+		warp: 0.05
+	}
 
+	/**
+	 * Generates the svg path string expression representing a curve.
+	 * @param {*} param0 
+	 * @param {*} param1 
+	 */
+    static calculate([ startX, startY ], [ endX, endY ]) {
 
-        // horizontal distance
         const Xdiff = Math.abs(endX - startX);
 
-        // M
-        const M = 'M' + startX + ',' + startY;
+		const M = `M ${startX},${startY}`;
 
-        // L
-        const L1 = 'L' + (Xdiff * Curve.warp + startX) + ',' + startY;
+        const L1 = `L ${Xdiff * Curve.warp + startX},${startY}`;
 
-        // C
         const c1 = startX + Xdiff * Curve.factor;
         const c2 = endX - Xdiff * Curve.factor;
         const c3 = - Xdiff * Curve.warp + endX;
-        const C = 'C' + c1 + ',' + startY + ' ' + c2 + ',' + endY + ' ' + c3 + ',' + endY
+        const C = `C ${c1},${startY} ${c2},${endY} ${c3},${endY}`;
 
-        // L
-        const L2 = 'L' + endX + ',' + endY;
+        const L2 = `L ${endX},${endY}`;
 
-        // setting up the path string
-        return M + ' ' + L1 + ' ' + C + ' ' + L2;
+		return `${M} ${L1} ${C} ${L2}`;
 
-    };
-
+	}
+	
+	/**
+	 * Gets the curve factor.
+	 */
     static get factor() {
 
-        return Curve.properties.factor;
+        return Curve.parameters.factor;
 
-    };
+    }
 
+	/**
+	 * Sets the curve factor.
+	 */
     static set factor(newFactor) {
 
-        Curve.properties.factor = newFactor;
+        Curve.parameters.factor = newFactor;
         Link.updateAll();
 
-    };
+    }
 
+	/**
+	 * Gets the curve's relative size.
+	 */
     static get warp() {
 
-        return Curve.properties.warp;
+        return Curve.parameters.warp;
 
-    };
+    }
 
+	/**
+	 * Sets the curve's relative size. 
+	 */
     static set warp(newWarp) {
 
-        Curve.properties.warp = newWarp;
+        Curve.parameters.warp = newWarp;
         Link.updateAll();
 
-    };
+    }
 
 }
-
-Curve.properties = {
-    factor: 0.4,
-    warp: 0.05
-};
