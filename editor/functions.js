073a8a617b179b895a03c6e5f609aaf49c9e039a
Ivan <ivan.lopes@live.fr>
2019-07-06 16:54:18 +0200

    added a new property to Array to get first element

--- editor/functions.js
+++ editor/functions.js
@@ -5,8 +5,21 @@
 // console.log handy shortcut
 const _ = (...a) => console.log(...a);
 
+// console.table handy shortcut
+const __ = (a) => console.table(a);
+
 // determine if a string contains either no characters or only spaces
 const isEmpty = str => !str.replace(/\s/g, '').length;
 
 // adds the given function to the JS queue of execution
 const wait = fn => setTimeout(fn, 0);
+
+// quick sugar getter to retrieve first element or Array
+Object.defineProperty(Array.prototype, 'first', {
+	get: function() {
+    	return this[0];
+    },
+	set: function(e) {
+		this[0] = e;
+	}
+});
