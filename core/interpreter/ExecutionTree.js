e7ff471eaf6843d9ccd8e05131a5959a7922589a
Ivan Lopes <ivan.lopes@live.fr>
2020-03-03 11:03:21 +0100

    [cg2020] added ET and Scope class

---
+++ core/interpreter/ExecutionTree.js
@@ -0,0 +1,12 @@
+'use strict'
+
+class ExecutionTree {
+
+  scope = new Scope();
+  accessBuffer = {};
+
+  constructor() {
+
+  }
+
+}
---
+++ core/interpreter/Scope.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class Scope {
+
+	constructor() {
+
+
+
+	}
+
+}
