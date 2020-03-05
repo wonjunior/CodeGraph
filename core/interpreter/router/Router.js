5322122c9174f86117f659e7d8749bedeacea427
Ivan Lopes <ivan.lopes@live.fr>
2020-03-05 11:34:43 +0100

    [cg2020] helper isExecutable function for routers

--- core/interpreter/router/Router.js
+++ core/interpreter/router/Router.js
@@ -4,6 +4,7 @@ class Router {
 
   in = [];
   out = [];
+  ET = null;
 
   get docks() {
 
@@ -16,5 +17,22 @@ class Router {
     return this.in.first.ancestor ? this.in.first.ancestor.router.root : this;
 
   }
+
+  execute() {
+
+    $_.log(`[R] UPDATING ${this.constructor.name}, tree root is ${this.root.constructor.name}`);
+    $_.indent();
+    $_.log(`(1) as an initialized ET = ${!!this.ET}`);
+    $_.unindent();
+
+  }
+
+  isExecutable() {
+
+    return !(this instanceof NullRouter);
+
+  }
+
+  header() {}
 
 }
